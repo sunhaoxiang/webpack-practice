65c6f5d95ac94feb5a98f4115ac1d42e2b453fb5
孙昊翔 <673686754@qq.com>
2018-03-29 10:57:47 +0800

    no message

--- CommonChunkPlugin.js
+++ CommonChunkPlugin.js
@@ -3,7 +3,9 @@ var path = require('path');
 
 module.export = {
   entry: {
-    pageA: './src/pageA'
+    pageA: './src/pageA',
+    pageB: './src/pageB',
+    vendor: ['lodash']  // 第三方库
   },
 
   output: {
@@ -13,9 +15,17 @@ module.export = {
   },
 
   plugin: [
+    // 打包自己业务代码的公共部分
     new webpack.optimize.CommonsChunkPlugin({
       name: 'common',
-      minChunks: 2
+      minChunks: 2, // 重复两次就提取出来
+      chunks: ['pageA', 'pageB'] // 指定打包公共代码的范围
+    }),
+    
+    // 打包第三方库和生成的代码
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+      minChunks: Infinity
     })
   ]
 }
