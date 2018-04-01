f0e25f5b64ff7b6b08d5dd0ecbd8f1176af422a9
孙昊翔 <673686754@qq.com>
2018-04-01 19:57:50 +0800

    no message

--- treeShaking.js
+++ treeShaking.js
@@ -12,6 +12,23 @@ module.export = {
     filename: '[name].bundle.js'
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+              plugins: ['lodash'] // 针对lodash的tree shaking
+            }
+          }
+        ]
+      }
+    ]
+  },
+
   plugins: [
     new webpack.optimize.UglifyJsPlugin({}) // 压缩并去除无用代码
   ]
