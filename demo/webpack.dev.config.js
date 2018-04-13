808af63649e31e7d83eeabbbd778ad7fc83e3f26
孙昊翔 <673686754@qq.com>
2018-04-13 16:22:29 +0800

    no message

---
+++ demo/webpack.dev.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const baseWebpackConfig = require('./webpack.base.config');
+const Html = require('html-webpack-plugin'); // 预设html模板
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const outputPath = path.join(__dirname, '../dist/client/');
+const templateSrc = path.join(__dirname, '../web/page/');
+module.exports = merge(baseWebpackConfig, {
+    devtool: 'source-map',
+    entry: {
+        admin: [
+            'eventsource-polyfill',
+            'webpack-hot-middleware/client',
+            '../web/page/admin/index.js',
+        ],
+        blog: [
+            'eventsource-polyfill',
+            'webpack-hot-middleware/client',
+            '../web/page/blog/index.js',
+        ]
+    },
+    output: {
+        path: outputPath,
+        publicPath: '/',
+        filename: 'js/[name].js',
+        chunkFilename: "js/[name].[chunkhash:8].js"
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('development')
+            }
+        }),
+        new Html({
+            filename: 'admin.html',
+            alwaysWriteToDisk: true,
+            template: path.join(templateSrc, '/admin/index.html'),
+            title: '<%= title || "游走在技术与艺术边缘地带的前端攻城狮" %>',
+            chunks: ["admin"],
+        }),
+        new Html({
+            filename: 'blog.html',
+            alwaysWriteToDisk: true,            
+            template: path.join(templateSrc, '/blog/index.html'),
+            html: '<%- html %>',
+            script: '<%- JSON.stringify(ServerData) %>',
+            title: '<%= title || "游走在技术与艺术边缘地带的前端攻城狮" %>',
+            chunks: ["blog"],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackHarddiskPlugin()
+    ]
+})
---
+++ demo/webpack.product.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const baseWebpackConfig = require('./webpack.base.config');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const Html = require('html-webpack-plugin'); // 预设html模板
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; // 打包结束后展示内容
+const templateSrc = path.join(__dirname, '../web/page/');
+const outputPath = path.join(__dirname, '../dist/client/');
+module.exports = merge(baseWebpackConfig, {
+    devtool: false,
+    entry: {
+        admin: '../web/page/admin/index.js',
+        blog: '../web/page/blog/index.js'
+    },
+    output: {
+        path: outputPath,
+        publicPath: '//static.liayal.com/',
+        filename: 'js/[name].[chunkhash:8].js',
+        chunkFilename: "js/[name].[chunkhash:8].js"
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+                DEBUG: false
+            }
+        }),
+        new Html({
+            filename: 'admin.html',
+            template: path.join(templateSrc, '/admin/index.html'),
+            chunks: ["admin"],
+            title: '<%= title || "游走在技术与艺术边缘地带的前端攻城狮" %>',
+        }),
+        new Html({
+            filename: 'blog.html',
+            template: path.join(templateSrc, '/blog/index.html'),
+            chunks: ["blog"],
+            html: '<%- html %>',
+            script: '<%- JSON.stringify(ServerData) %>',
+            title: '<%= title || "游走在技术与艺术边缘地带的前端攻城狮" %>',
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            // 最紧凑的输出
+            beautify: false,
+            // 删除所有的注释
+            comments: false,
+            compress: {
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false,
+                // 删除所有的 `console` 语句
+                // 还可以兼容ie浏览器
+                drop_console: true,
+                // 内嵌定义了但是只用到一次的变量
+                collapse_vars: true,
+                // 提取出出现多次但是没有定义成变量去引用的静态值
+                reduce_vars: true,
+            }
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new OptimizeCSSPlugin(),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new BundleAnalyzerPlugin()
+    ]
+});
